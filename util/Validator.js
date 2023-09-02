08e8728722d7ed751f0479a7c0dcfa89d14fb212
Abenezer <abenezergirma2@gmail.com>
2023-09-03 00:43:25 +0300

    Email and Phone Number Validation

--- util/Validator.js
+++ util/Validator.js
@@ -1,8 +1,24 @@
 const crypto = require("crypto")
 
 function isEmail(email) {
-	return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+    // check for aau.edu.et domain 
+	return /^[\w]+([\.-]?[\w]+)*@aau\.edu\.et$/.test(email);
 }
+
+/*
+const isValidEmail = isEmail("james_brown@abc.edu.e");
+console.log(isValidEmail);
+*/
+
+function isPhoneNumber(phoneNumber) {
+    // check for Telecom or Safaricom phone number
+	return /^(?:\+251|0)[97]\d{8}$/.test(phoneNumber);
+}
+
+/*
+const isValidPhoneNumber = isPhoneNumber("+251712345678");
+console.log(isValidPhoneNumber);
+*/
 
 function isName(name){
 	let isValid = (name.length > 3);
@@ -27,4 +43,4 @@ function isValidInitData (telegramInitData){
     return calculatedHash == hash;
 }
 
-module.exports = {isEmail, isName, isValidInitData}
+module.exports = {isEmail, isName, isPhoneNumber, isValidInitData}
